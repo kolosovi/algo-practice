1d3788f639da6b1b48c3ff8fecf6cc00f4302015
Ivan Kolosov <zackwag32@gmail.com>
2021-10-10 17:37:21 +0300

    gofmt all Go files (#3)

--- problems/leetcode/maximum-depth-of-n-ary-tree/go/narytree.go
+++ problems/leetcode/maximum-depth-of-n-ary-tree/go/narytree.go
@@ -1,38 +1,38 @@
 package narytree
 
 type Node struct {
-    Val int
-    Children []*Node
+	Val      int
+	Children []*Node
 }
 
 type PathItem struct {
-    Node *Node
-    ChildIdx int
+	Node     *Node
+	ChildIdx int
 }
 
 func maxDepth(root *Node) int {
-    if root == nil {
-        return 0
-    }
-    var result int
-    path := []PathItem{{Node: root, ChildIdx: 0}}
-    for len(path) > 0 {
-        cur := path[len(path) - 1]
-        if cur.ChildIdx >= len(cur.Node.Children) {
-            if cur.ChildIdx == 0 && len(path) > result {
-                result = len(path)
-            }
-            path = path[:len(path) - 1]
-            continue
-        }
-        path[len(path) - 1].ChildIdx++
-        path = append(
-            path,
-            PathItem{
-                Node: cur.Node.Children[cur.ChildIdx],
-                ChildIdx: 0,
-            },
-        )
-    }
-    return result
+	if root == nil {
+		return 0
+	}
+	var result int
+	path := []PathItem{{Node: root, ChildIdx: 0}}
+	for len(path) > 0 {
+		cur := path[len(path)-1]
+		if cur.ChildIdx >= len(cur.Node.Children) {
+			if cur.ChildIdx == 0 && len(path) > result {
+				result = len(path)
+			}
+			path = path[:len(path)-1]
+			continue
+		}
+		path[len(path)-1].ChildIdx++
+		path = append(
+			path,
+			PathItem{
+				Node:     cur.Node.Children[cur.ChildIdx],
+				ChildIdx: 0,
+			},
+		)
+	}
+	return result
 }

5168bbb39bb3372c9c58517a59c959da3b2e46ef
Ivan Kolosov <zackwag32@gmail.com>
2022-04-24 23:08:34 +0400

    Myself "Implement spinlock" solution (#40)

---
+++ problems/myself/implement-spinlock/cpp/spinlock.cpp
@@ -0,0 +1,19 @@
+#include "spinlock.hpp"
+
+#include <atomic>
+
+Spinlock::Spinlock() : is_locked_(false) {}
+
+void Spinlock::lock() {
+    while (is_locked_.exchange(true)) {
+    }
+}
+
+void Spinlock::unlock() {
+    is_locked_.store(false);
+}
+
+bool Spinlock::try_lock() {
+    bool expected = false;
+    return is_locked_.compare_exchange_strong(expected, true);
+}
---
+++ problems/myself/implement-spinlock/cpp/spinlock.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <atomic>
+
+class Spinlock {
+public:
+    Spinlock();
+
+    void lock();
+
+    void unlock();
+
+    bool try_lock();
+
+private:
+    std::atomic<bool> is_locked_;
+};

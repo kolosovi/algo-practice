953f6a5cdd3a5a95e6d13c48c1538b0c18ab07ca
Ivan Kolosov <zackwag32@gmail.com>
2022-05-12 23:32:13 +0400

    Chore: Add tests for Leetcode "Gas Station" (#56)

--- problems/leetcode/gas-station/cpp/solution.hpp
+++ problems/leetcode/gas-station/cpp/solution.hpp
@@ -1,6 +1,10 @@
+#pragma once
+
+#include <vector>
+
 class Solution {
 public:
-    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+    int canCompleteCircuit(std::vector<int>& gas, std::vector<int>& cost) {
         int balance = 0, steps = 0, start = 0, next = 0;
         while (start < gas.size()) {
             if (balance < 0) {

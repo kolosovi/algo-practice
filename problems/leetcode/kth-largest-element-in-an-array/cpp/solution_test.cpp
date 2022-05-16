e80759861929bd46b192ab322969ca111aad76f2
Ivan Kolosov <zackwag32@gmail.com>
2022-05-16 15:57:13 +0400

    Leetcode "Kth Largest Element in an Array" C++ solution (#61)
    Relates: https://leetcode.com/problems/kth-largest-element-in-an-array/

---
+++ problems/leetcode/kth-largest-element-in-an-array/cpp/solution_test.cpp
@@ -0,0 +1,11 @@
+#include <gtest/gtest.h>
+
+#include "solution.hpp"
+
+#include <vector>
+
+TEST(KthLargestElementInAnArrayTest, 1) {
+    Solution solution;
+    std::vector<int> nums{3,2,1,5,6,4};
+    EXPECT_EQ(solution.findKthLargest(nums, 2), 5);
+}

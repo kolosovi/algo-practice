92c086ee629c0a3c461650822a87c9787b5c8231
Ivan Kolosov <zackwag32@gmail.com>
2022-05-16 15:09:10 +0400

    Leetcode "Plus One" C++ solution (#60)
    Relates: https://leetcode.com/problems/plus-one/

---
+++ problems/leetcode/plus-one/cpp/solution.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <vector>
+
+class Solution {
+public:
+    std::vector<int> plusOne(std::vector<int>& digits) {
+        int carry = 0;
+        for (auto it = digits.rbegin(); it != digits.rend(); ++it) {
+            if (*it < 9) {
+                (*it)++;
+                break;
+            }
+            *it = 0;
+        }
+        std::vector<int> result(digits.size(), 0);
+        int i = 0;
+        if (digits[0] == 0) {
+            result.resize(result.size()+1);
+            result[i++] = 1;
+        }
+        for (auto digit : digits) {
+            result[i++] = digit;
+        }
+        return result;
+    }
+};
---
+++ problems/leetcode/plus-one/cpp/solution_test.cpp
@@ -0,0 +1,33 @@
+#include <gtest/gtest.h>
+
+#include "solution.hpp"
+
+#include <vector>
+
+TEST(PlusOneTest, 1) {
+    Solution solution;
+    std::vector<int> digits{0};
+    std::vector<int> expected{1};
+    EXPECT_EQ(solution.plusOne(digits), expected);
+}
+
+TEST(PlusOneTest, 2) {
+    Solution solution;
+    std::vector<int> digits{9};
+    std::vector<int> expected{1, 0};
+    EXPECT_EQ(solution.plusOne(digits), expected);
+}
+
+TEST(PlusOneTest, 3) {
+    Solution solution;
+    std::vector<int> digits{9, 9};
+    std::vector<int> expected{1, 0, 0};
+    EXPECT_EQ(solution.plusOne(digits), expected);
+}
+
+TEST(PlusOneTest, 4) {
+    Solution solution;
+    std::vector<int> digits{9, 8};
+    std::vector<int> expected{9, 9};
+    EXPECT_EQ(solution.plusOne(digits), expected);
+}

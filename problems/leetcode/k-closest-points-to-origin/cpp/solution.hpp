46f7b865675853ed7e04132758c3a0b5306bbd13
Ivan Kolosov <ikolosov@ozon.ru>
2022-05-17 14:20:35 +0400

    Leetcode "K Closest Points to Origin" C++ solution
    Relates: https://leetcode.com/problems/k-closest-points-to-origin/

---
+++ problems/leetcode/k-closest-points-to-origin/cpp/solution.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <algorithm>
+#include <vector>
+
+class Less {
+public:
+  bool operator()(const std::vector<int> &lhs, const std::vector<int> &rhs) {
+      return calcDistance(lhs) < calcDistance(rhs);
+  }
+
+private:
+  static inline int calcDistance(const std::vector<int> &point) {
+      return point[0] * point[0] + point[1] * point[1];
+  }
+};
+
+class Solution {
+public:
+    std::vector<std::vector<int>> kClosest(std::vector<std::vector<int>>& points, int k) {
+        Less less;
+        std::vector<std::vector<int>> heap;
+        for (const auto &point : points) {
+            heap.push_back(point);
+            std::push_heap(heap.begin(), heap.end(), less);
+            if (heap.size() > k) {
+                std::pop_heap(heap.begin(), heap.end(), less);
+                heap.pop_back();
+            }
+        }
+        return heap;
+    }
+};

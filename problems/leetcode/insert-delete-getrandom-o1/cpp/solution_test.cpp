55808cb378aa3eee256ef16fa5ed3e0b24cf508b
Ivan Kolosov <zackwag32@gmail.com>
2022-05-19 01:40:41 +0400

    Leetcode "Insert Delete GetRandom O(1)" C++ solution (#64)
    Relates: https://leetcode.com/problems/insert-delete-getrandom-o1/

---
+++ problems/leetcode/insert-delete-getrandom-o1/cpp/solution_test.cpp
@@ -0,0 +1,21 @@
+#include <gtest/gtest.h>
+
+#include "solution.hpp"
+
+#include <random>
+
+namespace {
+    static const int kSeed = 12345;
+}
+
+TEST(RandomizedSetTest, 1) {
+    std::mt19937 rng(kSeed);
+    RandomizedSet set(rng);
+    EXPECT_TRUE(set.insert(1));
+    EXPECT_FALSE(set.remove(2));
+    EXPECT_TRUE(set.insert(2));
+    EXPECT_EQ(set.getRandom(), 1);
+    EXPECT_TRUE(set.remove(1));
+    EXPECT_FALSE(set.insert(2));
+    EXPECT_EQ(set.getRandom(), 2);
+}

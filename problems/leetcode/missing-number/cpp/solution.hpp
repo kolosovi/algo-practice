81bc2b5499da6a01d2751f26dcaa8b1239973490
Ivan Kolosov <zackwag32@gmail.com>
2022-05-28 17:48:47 +0400

    Leetcode "Missing Number" C++ solution
    Relates: https://leetcode.com/problems/missing-number/

---
+++ problems/leetcode/missing-number/cpp/solution.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <vector>
+
+class Solution {
+public:
+    int missingNumber(std::vector<int>& nums) {
+        int expected_total = (nums.size() * (nums.size() + 1)) / 2;
+        int total = 0;
+        for (auto num : nums) {
+            total += num;
+        }
+        return expected_total - total;
+    }
+};

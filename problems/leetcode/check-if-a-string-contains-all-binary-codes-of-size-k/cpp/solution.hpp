6299684fbca9199254b67bba70dbeb8fb64189e4
Ivan Kolosov <ikolosov@ozon.ru>
2022-05-31 20:33:03 +0400

    Leetcode "Check If a String Contains All Binary Codes of Size K" C++ solution
    Relates: https://leetcode.com/problems/check-if-a-string-contains-all-binary-codes-of-size-k/

---
+++ problems/leetcode/check-if-a-string-contains-all-binary-codes-of-size-k/cpp/solution.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <cstdint>
+#include <string>
+#include <vector>
+
+class Solution {
+public:
+    bool hasAllCodes(std::string s, int k) {
+        uint32_t remaining_codes = (1 << k);
+        uint32_t mask = remaining_codes - 1;
+        std::vector<bool> has_code(remaining_codes, false);
+        uint32_t code = 0;
+        for (int i = 0; i < s.size(); i++) {
+            code = mask & ((code << 1) | (s[i] - '0'));
+            if (i >= k-1 && !has_code[code]) {
+                has_code[code] = true;
+                remaining_codes--;
+            }
+        }
+        return remaining_codes == 0;
+    }
+};

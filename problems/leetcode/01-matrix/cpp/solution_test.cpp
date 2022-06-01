efb7b26cac2ae03af033b96d9a0aa2f2f3c14181
Ivan Kolosov <zackwag32@gmail.com>
2022-06-01 09:59:11 +0400

    Leetcode "01 Matrix" C++ solution
    Relates: https://leetcode.com/problems/01-matrix/

---
+++ problems/leetcode/01-matrix/cpp/solution_test.cpp
@@ -0,0 +1,12 @@
+#include <gtest/gtest.h>
+
+#include "solution.hpp"
+
+#include <vector>
+
+TEST(Problem01MatrixTest, 1) {
+    Solution solution;
+    std::vector<std::vector<int>> input{{0,0,0},{0,1,0},{0,0,0}};
+    std::vector<std::vector<int>> expected{{0,0,0},{0,1,0},{0,0,0}};
+    EXPECT_EQ(solution.updateMatrix(input), expected);
+}

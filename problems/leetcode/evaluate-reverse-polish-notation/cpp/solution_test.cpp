add23bcf235897a4d52237100c1da09aecc2e9d1
Ivan Kolosov <zackwag32@gmail.com>
2022-06-04 22:05:01 +0400

    Leetcode "Evaluate Reverse Polish Notation" C++ solution (#77)
    Relates: https://leetcode.com/problems/evaluate-reverse-polish-notation/

---
+++ problems/leetcode/evaluate-reverse-polish-notation/cpp/solution_test.cpp
@@ -0,0 +1,27 @@
+#include <gtest/gtest.h>
+
+#include "solution.hpp"
+
+#include <vector>
+#include <string>
+
+TEST(EvalRPNTest, 1) {
+    Solution solution;
+    std::vector<std::string> tokens = {"2", "1", "+", "3", "*"};
+    int expected = 9;
+    EXPECT_EQ(solution.evalRPN(tokens), expected);
+}
+
+TEST(EvalRPNTest, 2) {
+    Solution solution;
+    std::vector<std::string> tokens = {"4", "13", "5", "/", "+"};
+    int expected = 6;
+    EXPECT_EQ(solution.evalRPN(tokens), expected);
+}
+
+TEST(EvalRPNTest, 3) {
+    Solution solution;
+    std::vector<std::string> tokens = {"10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+"};
+    int expected = 22;
+    EXPECT_EQ(solution.evalRPN(tokens), expected);
+}

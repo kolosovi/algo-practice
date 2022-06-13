48ba658ddb34f91b82f1bbda79eda35fe83e75bd
Ivan Kolosov <ikolosov@ozon.ru>
2022-06-13 10:27:41 +0300

    Leetcode "Coin Change" C++ solution
    Relates: https://leetcode.com/problems/coin-change/

---
+++ problems/leetcode/coin-change/cpp/solution_test.cpp
@@ -0,0 +1,23 @@
+#include <gtest/gtest.h>
+
+#include "solution.hpp"
+
+#include <vector>
+
+TEST(CoinChangeTest, 1) {
+    Solution solution;
+    std::vector<int> coins{2};
+    EXPECT_EQ(solution.coinChange(coins, 3), -1);
+}
+
+TEST(CoinChangeTest, 2) {
+    Solution solution;
+    std::vector<int> coins{1};
+    EXPECT_EQ(solution.coinChange(coins, 0), 0);
+}
+
+TEST(CoinChangeTest, 3) {
+    Solution solution;
+    std::vector<int> coins{1, 2, 5};
+    EXPECT_EQ(solution.coinChange(coins, 11), 3);
+}

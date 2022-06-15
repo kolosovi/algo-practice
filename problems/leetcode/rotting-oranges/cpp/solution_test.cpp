dba39230da77d3dc9146410e176eb4ded69c0844
Ivan Kolosov <ikolosov@ozon.ru>
2022-06-15 09:30:38 +0300

    Leetcode "Rotting Oranges" C++ solution
    Relates: https://leetcode.com/problems/rotting-oranges

---
+++ problems/leetcode/rotting-oranges/cpp/solution_test.cpp
@@ -0,0 +1,17 @@
+#include <gtest/gtest.h>
+
+#include "solution.hpp"
+
+#include <vector>
+
+TEST(OrangesRottingTest, 1) {
+    Solution solution;
+    std::vector<std::vector<int>> grid{{0, 1}};
+    EXPECT_EQ(solution.orangesRotting(grid), -1);
+}
+
+TEST(OrangesRottingTest, 2) {
+    Solution solution;
+    std::vector<std::vector<int>> grid{{2,1,1},{0,1,1},{1,0,1}};
+    EXPECT_EQ(solution.orangesRotting(grid), -1);
+}

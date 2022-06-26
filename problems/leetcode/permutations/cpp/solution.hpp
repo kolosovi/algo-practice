b7a7e2ecebfa97eeb52208b0889d0191e101828f
Ivan Kolosov <zackwag32@gmail.com>
2022-06-26 16:11:47 +0300

    Leetcode "Permutations" C++ solution (#86)
    Relates: https://leetcode.com/problems/permutations/

---
+++ problems/leetcode/permutations/cpp/solution.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <vector>
+#include <algorithm>
+
+class Solution {
+public:
+    std::vector<std::vector<int>> permute(std::vector<int>& nums) {
+        std::vector<std::vector<int>> result;
+        std::sort(nums.begin(), nums.end());
+        while (true) {
+            result.push_back(nums);
+            int pivot = -1;
+            for (int i = nums.size() - 1; i > 0; i--) {
+                if (nums[i-1] < nums[i]) {
+                    pivot = i-1;
+                    break;
+                }
+            }
+            if (pivot < 0) {
+                break;
+            }
+            int min_i = -1;
+            for (int i = pivot+1; i < nums.size(); i++) {
+                if (nums[i] < nums[pivot]) {
+                    continue;
+                }
+                if (min_i == -1 || nums[i] < nums[min_i]) {
+                    min_i = i;
+                }
+            }
+            std::swap(nums[pivot], nums[min_i]);
+            for (int l = pivot+1, r = nums.size()-1; l < r; l++, r--) {
+                std::swap(nums[l], nums[r]);
+            }
+        }
+        return result;
+    }
+};

d5cc42f77ac63be9156c636e4626a6ad43be22f2
Ivan Kolosov <zackwag32@gmail.com>
2022-07-02 13:50:52 +0300

    Leetcode "Sort Colors" C++ solution (#89)
    Relates: https://leetcode.com/problems/sort-colors/

---
+++ problems/leetcode/sort-colors/cpp/solution.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <vector>
+
+class Solution {
+public:
+    void sortColors(std::vector<int>& nums) {
+        int l = 0, r = nums.size() - 1, i = 0;
+        while (i <= r) {
+            if (nums[i] == 0) {
+                std::swap(nums[i++], nums[l++]);
+            } else if (nums[i] == 2) {
+                std::swap(nums[i], nums[r--]);
+                if (nums[i] == 1) {
+                    i++;
+                }
+            } else {
+                i++;
+            }
+        }
+    }
+};

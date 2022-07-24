90a0f7552f461c6dd834281eacbd9eae3393f9ed
Ivan Kolosov <zackwag32@gmail.com>
2022-07-24 14:33:30 +0300

    Leetcode "Unique Paths" C++ solution
    Relates: https://leetcode.com/problems/unique-paths

---
+++ problems/leetcode/unique-paths/cpp/solution_test.cpp
@@ -0,0 +1,25 @@
+#include <gtest/gtest.h>
+
+#include "solution.hpp"
+
+#include <vector>
+
+TEST(UniquePathsTest, 1) {
+    Solution solution;
+    EXPECT_EQ(solution.uniquePaths(1, 1), 1);
+}
+
+TEST(UniquePathsTest, 2) {
+    Solution solution;
+    EXPECT_EQ(solution.uniquePaths(3, 7), 28);
+}
+
+TEST(UniquePathsTest, 3) {
+    Solution solution;
+    EXPECT_EQ(solution.uniquePaths(3, 2), 3);
+}
+
+TEST(UniquePathsTest, 4) {
+    Solution solution;
+    EXPECT_EQ(solution.uniquePaths(6, 3), 21);
+}

5ce0e97627509e5471e269af14559572a2ed043f
Ivan Kolosov <zackwag32@gmail.com>
2022-08-05 00:03:57 +0300

    Leetcode "Binary Tree Right Side View" C++ solution (#100)
    Relates: https://leetcode.com/problems/binary-tree-right-side-view/

---
+++ problems/leetcode/binary-tree-right-side-view/cpp/solution_test.cpp
@@ -0,0 +1,12 @@
+#include <gtest/gtest.h>
+
+#include "solution.hpp"
+
+#include <vector>
+
+TEST(RightSideViewTest, 1) {
+    Solution solution;
+    auto *root = new TreeNode(42);
+    std::vector<int> expected{42};
+    EXPECT_EQ(solution.rightSideView(root), expected);
+}

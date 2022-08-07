785d5bfc26a3a9bad365c251846a44c05eb457e9
Ivan Kolosov <zackwag32@gmail.com>
2022-08-07 19:19:17 +0300

    Leetcode "Word Search" C++ solution (#103)
    Relates: https://leetcode.com/problems/word-search/

---
+++ problems/leetcode/word-search/cpp/solution_test.cpp
@@ -0,0 +1,17 @@
+#include <gtest/gtest.h>
+
+#include "solution.hpp"
+
+#include <string>
+#include <vector>
+
+TEST(ExistTest, 1) {
+    Solution solution;
+    std::vector<std::vector<char>> board{
+        {'A', 'B', 'C', 'E'},
+        {'S', 'F', 'C', 'S'},
+        {'A', 'D', 'E', 'E'}
+    };
+    std::string word{"ABCCED"};
+    EXPECT_TRUE(solution.exist(board, word));
+}

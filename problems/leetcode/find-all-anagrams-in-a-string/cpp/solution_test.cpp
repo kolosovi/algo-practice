b65e437a09f9da92d8111ed80db41acc63ff2bb9
Ivan Kolosov <zackwag32@gmail.com>
2022-08-12 23:33:30 +0300

    Leetcode "Find All Anagrams in a String" C++ solution (#106)
    Relates: https://leetcode.com/problems/find-all-anagrams-in-a-string/

---
+++ problems/leetcode/find-all-anagrams-in-a-string/cpp/solution_test.cpp
@@ -0,0 +1,11 @@
+#include <gtest/gtest.h>
+
+#include "solution.hpp"
+
+#include <vector>
+
+TEST(FindAnagramsTest, 1) {
+    Solution solution;
+    std::vector<int> expected{0, 6};
+    EXPECT_EQ(solution.findAnagrams("cbaebabacd", "abc"), expected);
+}

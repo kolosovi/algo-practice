0f6f1368d1d310a61bf492590ea51efddec46116
Ivan Kolosov <zackwag32@gmail.com>
2022-08-15 21:54:27 +0300

    Leetcode "Task Scheduler" C++ solution (#107)
    Relates: https://leetcode.com/problems/task-scheduler/

---
+++ problems/leetcode/task-scheduler/cpp/solution.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <vector>
+#include <algorithm>
+
+class Solution {
+public:
+    int leastInterval(std::vector<char>& tasks, int n) {
+        std::vector<int> freqs(26, 0);
+        for (const auto &task : tasks) {
+            freqs[task - 'A']++;
+        }
+        std::sort(freqs.begin(), freqs.end());
+        auto idle_slots = n * (freqs.back() - 1);
+        for (auto it = std::next(freqs.rbegin()); it != freqs.rend() && idle_slots > 0; ++it) {
+            idle_slots -= std::min(*it, freqs.back() - 1);
+        }
+        return tasks.size() + std::max(0, idle_slots);
+    }
+};

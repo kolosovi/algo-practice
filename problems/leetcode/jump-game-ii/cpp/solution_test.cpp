fbf40412aac0d774bb93c42e9a1ff99cbca74a85
Ivan Kolosov <zackwag32@gmail.com>
2022-09-08 20:49:19 +0400

    Leetcode "Jump Game II" C++ solution
    Relates: https://leetcode.com/problems/jump-game-ii/

---
+++ problems/leetcode/jump-game-ii/cpp/solution_test.cpp
@@ -0,0 +1,11 @@
+#include <gtest/gtest.h>
+
+#include "solution.hpp"
+
+#include <vector>
+
+TEST(JumpTest, 1) {
+    Solution solution;
+    std::vector<int> nums{2, 3, 1, 1, 4};
+    EXPECT_EQ(solution.jump(nums), 2);
+}

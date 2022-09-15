470d0189b8f3198c2255e3f44c35eebfab2e8297
Ivan Kolosov <zackwag32@gmail.com>
2022-09-15 15:05:09 +0400

    Add pre-commit hook that checks code formatting (#117)

--- scripts/cpp-format.sh
+++ scripts/cpp-format.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 
+IS_DRY_RUN=0
+EXIT_CODE=0
+
+while getopts ":n" option
+do
+    case $option in
+        n ) IS_DRY_RUN=1
+    esac
+done
+
 for name in `find -E problems -regex '.*\.hpp|.*\.cpp'`
 do
-	clang-format --verbose -i "$name"
+    if [[ $IS_DRY_RUN -eq 1 ]]
+    then
+        clang-format -Werror -n "$name" || let "EXIT_CODE = 1"
+    else
+        clang-format --verbose -i "$name"
+    fi
 done
+
+exit $EXIT_CODE

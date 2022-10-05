8864331830080b018689337f38c22fb6f5d8848f
Ivan Kolosov <zackwag32@gmail.com>
2022-10-05 18:51:18 +0400

    CSES "Missing Number" C++ solution (#139)
    Relates: https://cses.fi/problemset/task/1083/

---
+++ problems/laaks/cses-missing-number/cpp/main.cpp
@@ -0,0 +1,20 @@
+// This code was generated, do not edit.
+
+#include <iostream>
+
+#include <istream>
+#include <ostream>
+#include <cstdint>
+
+void Solve(std::istream &in, std::ostream &out) {
+  int64_t n;
+  in >> n;
+  int64_t result = ((1 + n) * n) / 2, num;
+  while (n-- > 1) {
+    in >> num;
+    result -= num;
+  }
+  out << result << '\n';
+}
+
+int main() { Solve(std::cin, std::cout); }

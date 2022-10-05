d4cd625b44c50adcfe7a26b09def271c17c3c76e
Ivan Kolosov <zackwag32@gmail.com>
2022-10-06 00:37:30 +0400

    CSES "Permutations" C++ solution (#142)
    Relates: https://cses.fi/problemset/task/1070/

---
+++ problems/laaks/cses-permutations/cpp/solution.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <istream>
+#include <ostream>
+
+void Solve(std::istream &in, std::ostream &out) {
+  int n;
+  in >> n;
+  if (n == 1) {
+    out << "1\n";
+    return;
+  }
+  if (n <= 3) {
+    out << "NO SOLUTION\n";
+    return;
+  }
+  if (n == 4) {
+    out << "2 4 1 3\n";
+    return;
+  }
+  for (int i = 1; i <= n; i += 2) {
+    out << i << ' ';
+  }
+  for (int i = 2; i <= n; i += 2) {
+    out << i;
+    if (i + 2 <= n) {
+      out << ' ';
+    }
+  }
+  out << '\n';
+}

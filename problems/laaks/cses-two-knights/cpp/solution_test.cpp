8c7170d1e76f4f672a40fd8674ba2686f1caa681
Ivan Kolosov <zackwag32@gmail.com>
2022-10-05 10:07:31 +0400

    CSES "Two Knights" C++ solution
    Relates: https://cses.fi/problemset/task/1072

---
+++ problems/laaks/cses-two-knights/cpp/solution_test.cpp
@@ -0,0 +1,15 @@
+#include <gtest/gtest.h>
+
+#include "solution.hpp"
+
+#include "testutil/file.hpp"
+
+TEST(TestLaaksCSESTwoKnights, 1) {
+  EXPECT_EQ(
+      "", testutil::FileBasedTest(Solve, __FILE__, "1.txt", "1_expected.txt"));
+}
+
+TEST(TestLaaksCSESTwoKnights, 2) {
+  EXPECT_EQ(
+      "", testutil::FileBasedTest(Solve, __FILE__, "2.txt", "2_expected.txt"));
+}

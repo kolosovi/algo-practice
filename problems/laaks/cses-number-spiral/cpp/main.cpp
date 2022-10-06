c888244dbab3ebad3c5453b237559ed0974bf1dc
Ivan Kolosov <ikolosov@ozon.ru>
2022-10-06 20:46:13 +0400

    CSES "Number Spiral" C++ solution
    Relates: https://cses.fi/problemset/task/1071/

---
+++ problems/laaks/cses-number-spiral/cpp/main.cpp
@@ -0,0 +1,34 @@
+// This code was generated, do not edit.
+
+#include <iostream>
+
+#include <istream>
+#include <ostream>
+#include <cstdint>
+#include <algorithm>
+
+void Solve(std::istream &in, std::ostream &out) {
+  int64_t t, row, col;
+  in >> t;
+  while (t-- > 0) {
+    in >> row >> col;
+    auto max = std::max(row, col);
+    int64_t res = (max - 1) * (max - 1);
+    if (max % 2 == 1) {
+      if (row >= col) {
+        res += col;
+      } else {
+        res += 2 * max - row;
+      }
+    } else {
+      if (row >= col) {
+        res += 2 * max - col;
+      } else {
+        res += row;
+      }
+    }
+    out << res << '\n';
+  }
+}
+
+int main() { Solve(std::cin, std::cout); }

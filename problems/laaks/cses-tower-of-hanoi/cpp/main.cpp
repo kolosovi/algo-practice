67f0172667a428acf20af6dbe3419f5a5f4af112
Ivan Kolosov <zackwag32@gmail.com>
2022-10-12 02:17:35 +0400

    CSES "Tower of Hanoi" C++ solution (#151)
    Relates: https://cses.fi/problemset/task/2165/

---
+++ problems/laaks/cses-tower-of-hanoi/cpp/main.cpp
@@ -0,0 +1,29 @@
+// This code was generated, do not edit.
+
+#include <iostream>
+
+#include <array>
+#include <istream>
+#include <ostream>
+
+static constexpr int kL = 1;
+static constexpr int kM = 2;
+static constexpr int kR = 3;
+
+void solve(std::ostream &out, std::array<int, 3> map, int n) {
+  if (n == 0) {
+    return;
+  }
+  solve(out, {map[kL - 1], map[kR - 1], map[kM - 1]}, n - 1);
+  out << map[kL - 1] << ' ' << map[kR - 1] << '\n';
+  solve(out, {map[kM - 1], map[kL - 1], map[kR - 1]}, n - 1);
+}
+
+void Solve(std::istream &in, std::ostream &out) {
+  int n;
+  in >> n;
+  out << ((1 << n) - 1) << '\n';
+  solve(out, {kL, kM, kR}, n);
+}
+
+int main() { Solve(std::cin, std::cout); }

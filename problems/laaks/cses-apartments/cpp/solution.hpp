ef0ceb9ee165fe6b2ea1b46fe504b5a62544f5fe
Ivan Kolosov <ikolosov@ozon.ru>
2022-10-18 22:43:13 +0400

    CSES "Apartments" C++ solution
    Relates: https://cses.fi/problemset/task/1084/

---
+++ problems/laaks/cses-apartments/cpp/solution.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <istream>
+#include <ostream>
+#include <algorithm>
+#include <vector>
+
+void Solve(std::istream &in, std::ostream &out) {
+  int num_applicants, num_apartments, max_diff;
+  in >> num_applicants >> num_apartments >> max_diff;
+  std::vector<int> desired(num_applicants);
+  std::vector<int> sizes(num_apartments);
+  for (int i = 0; i < num_applicants; i++) {
+    in >> desired[i];
+  }
+  for (int i = 0; i < num_apartments; i++) {
+    in >> sizes[i];
+  }
+  std::sort(desired.begin(), desired.end(), std::greater<int>{});
+  std::sort(sizes.begin(), sizes.end(), std::greater<int>{});
+  int count = 0, i = 0;
+  for (auto size : sizes) {
+    for (; i < desired.size() && desired[i] - max_diff > size; ++i) {
+    }
+    if (i == desired.size() || desired[i] + max_diff < size) {
+      continue;
+    }
+    count++;
+    i++;
+  }
+  out << count << '\n';
+}

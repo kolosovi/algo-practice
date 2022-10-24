d4abe3b46a0588a10f6c85c5f69b814320f85b6f
Ivan Kolosov <ikolosov@ozon.ru>
2022-10-24 23:10:03 +0400

    CSES "Movie Festival" C++ solution
    Relates: https://cses.fi/problemset/task/1629

---
+++ problems/laaks/cses-movie-festival/cpp/solution.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <algorithm>
+#include <istream>
+#include <ostream>
+#include <vector>
+#include <utility>
+
+void Solve(std::istream &in, std::ostream &out) {
+  int n;
+  in >> n;
+  std::vector<std::pair<int, int>> movies;
+  movies.reserve(n);
+  while (n-- > 0) {
+    movies.emplace_back();
+    in >> movies.back().first;
+    in >> movies.back().second;
+  }
+  std::sort(movies.begin(), movies.end(),
+            [](const std::pair<int, int> &lhs, const std::pair<int, int> &rhs) {
+              return lhs.second < rhs.second;
+            });
+  int i = 0, count = 0;
+  while (i < movies.size()) {
+    int j = i + 1;
+    for (; j < movies.size() && movies[j].first < movies[i].second; j++) {
+    }
+    count++;
+    i = j;
+  }
+  out << count << '\n';
+}

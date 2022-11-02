b317f62a94b51ac7600e870c44ebb8621c751cb1
Ivan Kolosov <zackwag32@gmail.com>
2022-11-03 00:14:03 +0400

    CSES "Collecting Number II" C++ solution (#163)
    Relates: https://cses.fi/problemset/task/2217/

---
+++ problems/laaks/cses-collecting-numbers-ii/cpp/solution_test.cpp
@@ -0,0 +1,10 @@
+#include <gtest/gtest.h>
+
+#include <testutil/file.hpp>
+
+#include "solution.hpp"
+
+TEST(TestLaaksCSESCollectingNumbersII, 1) {
+  EXPECT_EQ(
+      "", testutil::FileBasedTest(Solve, __FILE__, "1.txt", "1_expected.txt"));
+}

b15bd7538d5842833437c4b3c4c2d7e10c38dc74
Ivan Kolosov <zackwag32@gmail.com>
2023-02-27 20:52:20 +0300

    CSES "Tasks and Deadlines" C++ solution (#176)
    Relates: https://cses.fi/problemset/task/1630/

---
+++ problems/laaks/cses-tasks-and-deadlines/cpp/solution.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <istream>
+#include <ostream>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <cstdint>
+
+void Solve(std::istream &in, std::ostream &out) {
+  int n;
+  int64_t elapsed = 0, reward = 0;
+  in >> n;
+  std::vector<int> durations(n, 0);
+  std::vector<int> deadlines(n, 0);
+  std::vector<int> tasks(n, 0);
+  std::iota(tasks.begin(), tasks.end(), 0);
+  for (int i = 0; i < n; i++) {
+    in >> durations[i];
+    in >> deadlines[i];
+  }
+  std::sort(tasks.begin(), tasks.end(),
+            [&durations](const int &lhs, const int &rhs) {
+              return durations[lhs] < durations[rhs];
+            });
+  for (auto task : tasks) {
+    elapsed += static_cast<int64_t>(durations[task]);
+    reward += static_cast<int64_t>(deadlines[task]) - elapsed;
+  }
+  out << reward << '\n';
+}

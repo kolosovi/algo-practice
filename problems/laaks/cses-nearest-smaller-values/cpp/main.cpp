bdad27eaca498e412fedb04aa2d9b7237735b07d
Ivan Kolosov <zackwag32@gmail.com>
2023-03-08 19:06:17 +0300

    CSES "Nearest Smaller Values" C++ solution (#181)
    Relates: https://cses.fi/problemset/task/1645/

---
+++ problems/laaks/cses-nearest-smaller-values/cpp/main.cpp
@@ -0,0 +1,36 @@
+// This code was generated, do not edit.
+
+#include <iostream>
+
+#include <istream>
+#include <ostream>
+#include <vector>
+#include <algorithm>
+
+void Solve(std::istream &in, std::ostream &out) {
+  int n, num;
+  in >> n;
+  std::vector<int> candidate_indexes, candidates;
+  for (int i = 0; i < n; i++) {
+    in >> num;
+    auto past_answer_it =
+        std::lower_bound(candidates.begin(), candidates.end(), num);
+    if (candidates.size() > 0 && past_answer_it != candidates.begin()) {
+      out << candidate_indexes[past_answer_it - candidates.begin() - 1];
+    } else {
+      out << 0;
+    }
+    if (i != n - 1) {
+      out << ' ';
+    }
+    while (candidates.size() > 0 && candidates.back() >= num) {
+      candidates.pop_back();
+      candidate_indexes.pop_back();
+    }
+    candidates.push_back(num);
+    candidate_indexes.push_back(i + 1);
+  }
+  out << '\n';
+}
+
+int main() { Solve(std::cin, std::cout); }

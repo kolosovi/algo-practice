8dd7207d6205c433a2dc4f920978ef2f83bb0055
Ivan Kolosov <zackwag32@gmail.com>
2023-03-11 05:23:18 +0300

    CSES "Subarray Sums II" C++ solution (#183)
    Relates: https://cses.fi/problemset/task/1661/

---
+++ problems/laaks/cses-subarray-sums-ii/cpp/solution.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <cstdint>
+#include <istream>
+#include <ostream>
+#include <vector>
+#include <unordered_map>
+
+void Solve(std::istream &in, std::ostream &out) {
+  int n, target;
+  in >> n >> target;
+  int64_t sum = 0, result = 0, number;
+  std::unordered_map<int64_t, int64_t> sum_counts{{0, 1}};
+  for (int i = 0; i < n; i++) {
+    in >> number;
+    sum += number;
+    int64_t wanted = sum - target;
+    auto wanted_count_it = sum_counts.find(wanted);
+    if (wanted_count_it != sum_counts.end()) {
+      result += wanted_count_it->second;
+    }
+    sum_counts[sum]++;
+  }
+  out << result << '\n';
+}

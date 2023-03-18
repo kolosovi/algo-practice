6cf177699b13e020d8e1bc5a4e6807fbc919054c
Ivan Kolosov <zackwag32@gmail.com>
2023-03-18 17:43:16 +0300

    CSES "Playlist" C++ solution (fix) (#186)
    Fixes "time limit exceeded" errors.
    
    Relates: https://cses.fi/problemset/task/1141

--- problems/laaks/cses-playlist/cpp/solution_test.cpp
+++ problems/laaks/cses-playlist/cpp/solution_test.cpp
@@ -8,3 +8,8 @@ TEST(TestLaaksCSESPlaylist, 1) {
   EXPECT_EQ(
       "", testutil::FileBasedTest(Solve, __FILE__, "1.txt", "1_expected.txt"));
 }
+
+TEST(TestLaaksCSESPlaylist, 2) {
+  EXPECT_EQ(
+      "", testutil::FileBasedTest(Solve, __FILE__, "2.txt", "2_expected.txt"));
+}

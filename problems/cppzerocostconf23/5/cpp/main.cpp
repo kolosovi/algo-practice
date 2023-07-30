f480d741c8db59afe74da76b7516c070a168fdc8
Ivan Kolosov <zackwag32@gmail.com>
2023-07-30 14:56:00 +0300

    C++ Zero Cost Conf 2023 contest solutions (#189)

---
+++ problems/cppzerocostconf23/5/cpp/main.cpp
@@ -0,0 +1,22 @@
+// This code was generated, do not edit.
+
+#include <iostream>
+
+#include <istream>
+#include <ostream>
+
+static constexpr char *kLookupTable = "YANDEXTAXI";
+
+void Solve(std::istream &in, std::ostream &out) {
+  char c;
+  while (in.get(c)) {
+    if (c < '0' || c > '9') {
+      out << c;
+      continue;
+    }
+    out << (kLookupTable[c - '0']);
+  }
+  out << std::endl;
+}
+
+int main() { Solve(std::cin, std::cout); }

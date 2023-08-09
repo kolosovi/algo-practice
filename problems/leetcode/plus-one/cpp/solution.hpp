aeff07f1398f9998f5b18d829575ec8345a99a18
Ivan Kolosov <zackwag32@gmail.com>
2023-08-09 23:52:28 +0300

    Leetcode "Plus One" improved C++ solution (#195)
    Relates: https://leetcode.com/problems/plus-one/

--- problems/leetcode/plus-one/cpp/solution.hpp
+++ problems/leetcode/plus-one/cpp/solution.hpp
@@ -5,23 +5,15 @@
 class Solution {
  public:
   std::vector<int> plusOne(std::vector<int>& digits) {
-    int carry = 0;
-    for (auto it = digits.rbegin(); it != digits.rend(); ++it) {
-      if (*it < 9) {
-        (*it)++;
-        break;
+    for (int i = digits.size() - 1; i >= 0; i--) {
+      if (digits[i] < 9) {
+        digits[i]++;
+        return digits;
       }
-      *it = 0;
+      digits[i] = 0;
     }
-    std::vector<int> result(digits.size(), 0);
-    int i = 0;
-    if (digits[0] == 0) {
-      result.resize(result.size() + 1);
-      result[i++] = 1;
-    }
-    for (auto digit : digits) {
-      result[i++] = digit;
-    }
-    return result;
+    digits.push_back(0);
+    digits[0] = 1;
+    return digits;
   }
 };

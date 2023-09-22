ed21f19d767f06136833f16e800a1053328ba8b8
Ivan Kolosov <zackwag32@gmail.com>
2023-09-22 21:44:41 +0300

    CSES "Nested Ranges Count" C++ solution (#202)
    Relates: https://cses.fi/problemset/task/2169/

--- problems/laaks/cses-nested-ranges-count/cpp/solution.hpp
+++ problems/laaks/cses-nested-ranges-count/cpp/solution.hpp
@@ -7,260 +7,111 @@
 #include <utility>
 #include <vector>
 
-enum class Color : bool { kBlack = 0, kRed = 1 };
-
-template <class Key>
-struct Node {
-  Node<Key> *parent;
-  Node<Key> *left;
-  Node<Key> *right;
-  int size;
-  int factor;
-  Key key;
-  Color color;
-
-  Node()
-      : parent(nullptr),
-        left(nullptr),
-        right(nullptr),
-        size(0),
-        factor(0),
-        key(),
-        color(Color::kBlack) {}
-
-  Node(Key key, Node<Key> *parent, Node<Key> *left, Node<Key> *right)
-      : parent(parent),
-        left(left),
-        right(right),
-        size(1),
-        factor(1),
-        key(key),
-        color(Color::kRed) {}
-
-  bool IsRed() const { return color == Color::kRed; }
-};
-
-template <class Key, class Compare>
-class Set {
+class Segtree {
  public:
-  Set() : cmp_(), nil_(new Node<Key>()), root_(nil_) {}
-
-  void Insert(Key key) {
-    auto *parent = nil_;
-    auto *place = root_;
-    while (place != nil_) {
-      parent = place;
-      parent->size++;
-      if (cmp_(key, place->key)) {
-        place = place->left;
-      } else if (cmp_(place->key, key)) {
-        place = place->right;
-      } else {
-        place->factor++;
-        return;
-      }
+  Segtree(int l, int r)
+      : l_(l), r_(r), count_(0), left_(nullptr), right_(nullptr) {
+    if (r - l <= 1) {
+      return;
     }
-    auto *newbie = new Node<Key>(key, parent, nil_, nil_);
-    if (parent == nil_) {
-      root_ = newbie;
-    } else if (cmp_(newbie->key, parent->key)) {
-      parent->left = newbie;
+    auto m = l + (r - l) / 2;
+    left_ = new Segtree(l, m);
+    right_ = new Segtree(m, r);
+  }
+
+  void Inc(int i, int value) {
+    if (i < l_ || i >= r_) {
+      return;
+    }
+    count_ += value;
+    if (left_ == nullptr) {
+      return;
+    }
+    if (i < left_->r_) {
+      left_->Inc(i, value);
     } else {
-      parent->right = newbie;
+      right_->Inc(i, value);
     }
-    insertFixup(newbie);
   }
 
-  Node<Key> *UpperBound(Key key) const {
-    Node<Key> *last = nil_, *cur = root_;
-    while (cur != nil_) {
-      last = cur;
-      if (cmp_(key, cur->key)) {
-        cur = cur->left;
-      } else if (cmp_(cur->key, key)) {
-        cur = cur->right;
-      } else {
-        break;
-      }
+  int Count(int target_l, int target_r) const {
+    if (l_ >= target_l && r_ <= target_r) {
+      return count_;
     }
-    if (last == nil_ || cmp_(key, last->key)) {
-      return last;
+    if (left_ == nullptr) {
+      return 0;
     }
-    return succ(last);
-  }
-
-  int Rank(Node<Key> *node) const {
-    int rank = node->left->size;
-    for (auto *child = node; child != root_; child = child->parent) {
-      if (child == child->parent->right) {
-        rank += child->parent->left->size + child->parent->factor;
-      }
+    if (target_l >= r_ || l_ >= target_r) {
+      return 0;
     }
-    return rank;
-  }
-
-  int Size() const { return root_->size; }
-
-  int UpperBoundRank(Key key) const {
-    auto *bound = UpperBound(key);
-    if (bound != nil_) {
-      return Rank(bound);
-    }
-    return Size();
+    return left_->Count(target_l, target_r) + right_->Count(target_l, target_r);
   }
 
  private:
-  Compare cmp_;
-  Node<Key> *nil_;
-  Node<Key> *root_;
-
-  void insertFixup(Node<Key> *node) {
-    while (node->parent->IsRed()) {
-      if (node->parent == node->parent->parent->left) {
-        auto *uncle = node->parent->parent->right;
-        if (uncle->IsRed()) {
-          node->parent->color = Color::kBlack;
-          uncle->color = Color::kBlack;
-          node->parent->parent->color = Color::kRed;
-          node = node->parent->parent;
-        } else {
-          if (node == node->parent->right) {
-            node = node->parent;
-            leftRotate(node);
-          }
-          node->parent->color = Color::kBlack;
-          node->parent->parent->color = Color::kRed;
-          rightRotate(node->parent->parent);
-        }
-      } else {
-        auto *uncle = node->parent->parent->left;
-        if (uncle->IsRed()) {
-          node->parent->color = Color::kBlack;
-          uncle->color = Color::kBlack;
-          node->parent->parent->color = Color::kRed;
-          node = node->parent->parent;
-        } else {
-          if (node == node->parent->left) {
-            node = node->parent;
-            rightRotate(node);
-          }
-          node->parent->color = Color::kBlack;
-          node->parent->parent->color = Color::kRed;
-          leftRotate(node->parent->parent);
-        }
-      }
-    }
-    root_->color = Color::kBlack;
-  }
-
-  void leftRotate(Node<Key> *node) {
-    auto *replacement = node->right;
-    node->right = replacement->left;
-    replacement->size = node->size;
-    node->size = node->factor + node->left->size + node->right->size;
-    if (replacement->left != nil_) {
-      replacement->left->parent = node;
-    }
-    replacement->parent = node->parent;
-    if (node->parent == nil_) {
-      root_ = replacement;
-    } else if (node == node->parent->left) {
-      node->parent->left = replacement;
-    } else {
-      node->parent->right = replacement;
-    }
-    replacement->left = node;
-    node->parent = replacement;
-  }
-
-  void rightRotate(Node<Key> *node) {
-    auto *replacement = node->left;
-    node->left = replacement->right;
-    replacement->size = node->size;
-    node->size = node->factor + node->left->size + node->right->size;
-    if (replacement->right != nil_) {
-      replacement->right->parent = node;
-    }
-    replacement->parent = node->parent;
-    if (node->parent == nil_) {
-      root_ = replacement;
-    } else if (node == node->parent->left) {
-      node->parent->left = replacement;
-    } else {
-      node->parent->right = replacement;
-    }
-    replacement->right = node;
-    node->parent = replacement;
-  }
-
-  Node<Key> *succ(Node<Key> *last) const {
-    if (last->right != nil_) {
-      return min(last->right);
-    }
-    auto *parent = last->parent, *child = last;
-    while (parent != nil_ && parent->left != child) {
-      child = parent;
-      parent = parent->parent;
-    }
-    return parent;
-  }
-
-  Node<Key> *min(Node<Key> *node) const {
-    for (; node->left != nil_; node = node->left) {
-    }
-    return node;
-  }
+  int l_, r_, count_;
+  Segtree *left_, *right_;
 };
 
 void Solve(std::istream &in, std::ostream &out) {
   int n;
   in >> n;
-  std::vector<std::pair<int, int>> ranges;
   std::vector<int> indexes(n);
   std::iota(indexes.begin(), indexes.end(), 0);
-  std::vector<int> contained_in_counts(n, 0), contains_counts(n, 0);
+  std::vector<int> lower_bounds(n), upper_bounds(n);
   for (int i = 0; i < n; i++) {
-    std::pair<int, int> range;
-    in >> range.first >> range.second;
-    ranges.push_back(std::move(range));
+    in >> lower_bounds[i] >> upper_bounds[i];
   }
-  // Sort by lower end DESC, upper end ASC
   std::sort(indexes.begin(), indexes.end(),
-            [&ranges](const int &lhs_i, const int &rhs_i) {
-              const auto &lhs = ranges[lhs_i], &rhs = ranges[rhs_i];
-              if (lhs.first > rhs.first) {
-                return true;
+            [&lower_bounds, &upper_bounds](const int &i, const int &j) {
+              if (lower_bounds[i] == lower_bounds[j]) {
+                return upper_bounds[i] > upper_bounds[j];
               }
-              if (lhs.first == rhs.first) {
-                return lhs.second < rhs.second;
-              }
-              return false;
+              return lower_bounds[i] < lower_bounds[j];
             });
-  Set<int, std::less<int>> upper_ends_ASC;
-  for (auto it = indexes.begin(); it != indexes.end(); ++it) {
-    contains_counts[*it] = upper_ends_ASC.UpperBoundRank(ranges[*it].second);
-    upper_ends_ASC.Insert(ranges[*it].second);
+
+  // segtree will be built on unique upper bounds
+  std::vector<int> unique_upper_bounds(upper_bounds);
+  std::sort(unique_upper_bounds.begin(), unique_upper_bounds.end());
+  int last_unique_i = 0;
+  for (int i = 1; i < n; i++) {
+    if (unique_upper_bounds[i] != unique_upper_bounds[last_unique_i]) {
+      unique_upper_bounds[++last_unique_i] = unique_upper_bounds[i];
+    }
   }
-  Set<int, std::greater<int>> upper_ends_DESC;
-  // Visit ranges in lower end ASC, upper end DESC order
-  for (auto it = indexes.rbegin(); it != indexes.rend(); ++it) {
-    contained_in_counts[*it] =
-        upper_ends_DESC.UpperBoundRank(ranges[*it].second);
-    upper_ends_DESC.Insert(ranges[*it].second);
+  unique_upper_bounds.resize(last_unique_i + 1);
+  std::unordered_map<int, int> ub_to_i;
+  for (int i = 0; i < unique_upper_bounds.size(); i++) {
+    ub_to_i[unique_upper_bounds[i]] = i;
   }
-  for (auto it = contains_counts.begin(); it != contains_counts.end(); ++it) {
-    if (it != contains_counts.begin()) {
+
+  std::vector<int> includes_me_count(n, 0), included_by_me_count(n, 0);
+  Segtree ltr(0, unique_upper_bounds.size()),
+      rtl(0, unique_upper_bounds.size());
+  for (int i = 0; i < indexes.size(); i++) {
+    auto idx = indexes[i];
+    auto ub_i = ub_to_i[upper_bounds[idx]];
+    includes_me_count[idx] = ltr.Count(ub_i, unique_upper_bounds.size());
+    ltr.Inc(ub_i, 1);
+  }
+  for (int i = indexes.size() - 1; i >= 0; i--) {
+    auto idx = indexes[i];
+    auto ub_i = ub_to_i[upper_bounds[idx]];
+    included_by_me_count[idx] = rtl.Count(0, ub_i + 1);
+    rtl.Inc(ub_i, 1);
+  }
+
+  for (int idx = 0; idx < n; idx++) {
+    if (idx > 0) {
       out << ' ';
     }
-    out << *it;
+    out << included_by_me_count[idx];
   }
   out << '\n';
-  for (auto it = contained_in_counts.begin(); it != contained_in_counts.end();
-       ++it) {
-    if (it != contained_in_counts.begin()) {
+  for (int idx = 0; idx < n; idx++) {
+    if (idx > 0) {
       out << ' ';
     }
-    out << *it;
+    out << includes_me_count[idx];
   }
   out << '\n';
 }
